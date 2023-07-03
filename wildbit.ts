a6b8d9add04b3947b64ca843c0803e3d40485bfb
DoraLC <34760377+DoraLC@users.noreply.github.com>
2023-07-03 16:58:51 +0800

    updates

--- wildbit.ts
+++ wildbit.ts
@@ -4,37 +4,12 @@
 namespace wildbit {
 
     const PCA9685_ADDRESS = 0x40
-
     const MODE1 = 0x00
-    const MODE2 = 0x01
-
-    const SUBADR1 = 0x02
-    const SUBADR2 = 0x03
-    const SUBADR3 = 0x04
     const PRESCALE = 0xFE
-
     const LED0_ON_L = 0x06
-    const LED0_ON_H = 0x07
-    const LED0_OFF_L = 0x08
-    const LED0_OFF_H = 0x09
-
-    const ALL_LED_ON_L = 0xFA
-    const ALL_LED_ON_H = 0xFB
-    const ALL_LED_OFF_L = 0xFC
-    const ALL_LED_OFF_H = 0xFD
-
-    const STP_CHA_L = 2047
-    const STP_CHA_H = 4095
-    const STP_CHB_L = 1
-    const STP_CHB_H = 2047
-    const STP_CHC_L = 1023
-    const STP_CHC_H = 3071
-    const STP_CHD_L = 3071
-    const STP_CHD_H = 1023
 
     const on = 0
     const off = 0
-    const freq_hz = 60
 
     let initialized = false
     let neoStrip: neopixel.Strip;
